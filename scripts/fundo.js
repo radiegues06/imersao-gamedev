044d4422e2c3da876278ebad3f9140345929fb33
rafaelamanciodiegues@gmail.com <ri65%ma58R>
2020-06-23 18:29:37 -0300

    add: Classe de imagem de fundo
    Adiciona classe que gera uma imagem de fundo e implementa métodos para movê-la.

---
+++ scripts/fundo.js
@@ -0,0 +1,27 @@
+class Fundo {
+    constructor(imagePath, speed) {
+        this.img = loadImage(imagePath);
+        this.speed = speed;
+        this.setOriginalImagePositions();
+    }
+
+    setOriginalImagePositions() {
+        this.pos1 = 0;
+        this.pos2 = windowWidth;
+    }
+
+    drawFundo() {
+        image(this.img, this.pos1, 0, windowWidth, windowHeight);
+        image(this.img, this.pos2, 0, windowWidth, windowHeight);
+        this.moveImage();
+    }
+
+    moveImage() {
+        this.pos1 -= this.speed;
+        this.pos2 -= this.speed;
+
+        if (this.pos2 < 0) {
+            this.setOriginalImagePositions();
+        }
+    }
+}
--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
-function preload() {
+let backgroundImg;
 
-}
+function preload() {}
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
+    backgroundImg = new Fundo("assets/imagens/cenario/floresta.png", 3);
 }
 
 function draw() {
-    background(220);
+    backgroundImg.drawFundo();
 }

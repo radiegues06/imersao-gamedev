ec087c6d8c2d73d0dee7075e5ecad48ee749c4f4
rafaelamanciodiegues@gmail.com <ri65%ma58R>
2020-06-25 08:13:18 -0300

    add: Multiplos fundos e paralaxe
    Adiciona multiplas imagens de fundo, implementando efeito de paralaxe

--- sketch.js
+++ sketch.js
@@ -1,16 +1,22 @@
-let backgroundImg;
+let backgroundImages = [];
 let personagem;
 
 function preload() {}
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    backgroundImg = new Fundo("assets/imagens/cenario/floresta.png", 20);
+
+    backgroundImages[0] = new Fundo("assets/imagens/cenario/Sky.png", 0);
+    backgroundImages[1] = new Fundo("assets/imagens/cenario/BG_Decor.png", 2);
+    backgroundImages[2] = new Fundo("assets/imagens/cenario/Middle_Decor.png", 5);
+    backgroundImages[3] = new Fundo("assets/imagens/cenario/Foreground.png", 10);
+    backgroundImages[4] = new Fundo("assets/imagens/cenario/Ground.png", 18);
+
     personagem = new Personagem("assets/imagens/personagem/correndo.png", 880, 1080, 4, 4, 880 / 5, 1080 / 5);
-    frameRate(10);
+    frameRate(30);
 }
 
 function draw() {
-    backgroundImg.drawFundo();
+    backgroundImages.forEach(element => element.drawFundo());
     personagem.animate();
 }

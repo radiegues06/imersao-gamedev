127eef5956a60b3bc697e8367286258b6a452644
rafaelamanciodiegues@gmail.com <ri65%ma58R>
2020-06-25 11:48:30 -0300

    fix: Corrige carregamento parcial da página
    Realoca carregamento de imagens para a função de preload

--- sketch.js
+++ sketch.js
@@ -1,11 +1,7 @@
 let backgroundImages = [];
 let protagonista = {};
 
-function preload() {}
-
-function setup() {
-    createCanvas(windowWidth, windowHeight);
-
+function preload() {
     backgroundImages[0] = new Fundo("assets/imagens/cenario/Sky.png", 0);
     backgroundImages[1] = new Fundo("assets/imagens/cenario/BG_Decor.png", 2);
     backgroundImages[2] = new Fundo("assets/imagens/cenario/Middle_Decor.png", 5);
@@ -16,7 +12,10 @@ function setup() {
         new Sprite("assets/imagens/personagem/correndo.png", 880, 1080, 4, 4),
         new Screen(880 / 5, 1080 / 5, .07 * windowWidth, windowHeight - 1.05 * 1080 / 5)
     );
+}
 
+function setup() {
+    createCanvas(windowWidth, windowHeight);
     frameRate(30);
 }
 
